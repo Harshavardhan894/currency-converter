3a245d0677ffd674ce88658aa2bad96fbad5dcd0
Harshavardhan894 <akulaharshavardhan23@gmail.com>
2025-06-07 19:36:03 +0530

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,213 @@
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+let dropdowns=document.querySelectorAll(".dropdown select");
+for(let i =0;i<2 ;i++){
+  for(let code in countryList){
+    if(!(code=="USD" || code=="INR"));
+    let newoption = document.createElement("option");
+    newoption.innerText=code;
+    newoption.value  =code;
+    dropdowns[i].append(newoption);
+  } 
+}
+let country=document.getElementById("currency_from");
+let from="USD",to="INR";
+country.addEventListener("change",(event)=>{
+    let x=event.target.value;
+    from=x;
+    let image=document.getElementById("from-image");
+    let y=image.getAttribute("src");
+    y="https://flagsapi.com/"+countryList[x]+"/flat/64.png";
+    image.setAttribute("src",y);
+});
+let country_to=document.getElementById("currency_to");
+country_to.addEventListener("change",(event)=>{
+    let x=event.target.value;
+    to=x;
+    let image=document.getElementById("to-image");
+    let y=image.getAttribute("src");
+    y="https://flagsapi.com/"+countryList[x]+"/flat/64.png";
+    image.setAttribute("src",y);
+});
+let ans=document.getElementById("sub");
+URL="https://latest.currency-api.pages.dev/v1/currencies/eur.json";
+ans.addEventListener("click",async (event)=>{ 
+  event.preventDefault(); 
+  let amount=document.querySelector(".amount input");
+  let val=amount.value;
+  if(val==="" || val<0){
+    amount.value=1;
+    val=1;
+  }
+  to=to.toLowerCase();
+  from=from.toLowerCase();
+  let res=await fetch(URL);
+  let data=await res.json();
+  from_val=data.eur[from];
+  to_val=data.eur[to];
+  to=to.toUpperCase();
+  from=from.toUpperCase();
+  let outputval=val*to_val/from_val;
+  let output= val.toString() + ' ' +from+ ' is equal to '+ outputval.toString() +' '+ to;
+  let msg=document.querySelector(".msg");
+  msg.innerText=output;
+})
